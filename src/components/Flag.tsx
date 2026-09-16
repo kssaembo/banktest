3baa939172fa7587669faf485b2d53f9f4683d8b
kssaembo <sinjoppo@gmail.com>
2026-09-17 01:14:01 +0900

    Delete src directory

--- src/components/Flag.tsx
+++
@@ -1,7 +0,0 @@
-export function Flag({ code }: { code: string }) {
-  return /^[A-Z]{2}$/.test(code) ? (
-    <img className="country-flag" src={`/flags/${code}.svg`} alt={code} width="27" height="18" />
-  ) : (
-    <span aria-label={code}>🌐</span>
-  )
-}
